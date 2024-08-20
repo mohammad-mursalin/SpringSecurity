385df18ea32943c39403d42a29368c14457530b4
mohammad_mursalin <mursalinlamon@gmail.com>
2024-08-20 09:50:24 +0600

    getting session id and changing the username and password

--- src/main/java/com/example/SpringSecurity/controller/Hello.java
+++ src/main/java/com/example/SpringSecurity/controller/Hello.java
@@ -1,12 +1,15 @@
 package com.example.SpringSecurity.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Hello {
+
     @RequestMapping("/")
-    public String greet() {
-        return "Assalamualaikum everyone...";
+    public String greet(HttpServletRequest request) {
+
+        return "Assalamualaikum everyone..." + request.getSession().getId();
     }
 }
