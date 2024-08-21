b0c685679f651c862360e64577b1178de5b9156e
mohammad_mursalin <mursalinlamon@gmail.com>
2024-08-21 23:38:55 +0600

    bcrypt password encoder

---
+++ src/main/java/com/example/SpringSecurity/service/UserService.java
@@ -0,0 +1,23 @@
+package com.example.SpringSecurity.service;
+
+import com.example.SpringSecurity.model.Users;
+import com.example.SpringSecurity.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepo repo;
+
+    private BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(12);
+
+    public Users register(Users user) {
+
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+
+        return repo.save(user);
+    }
+}
